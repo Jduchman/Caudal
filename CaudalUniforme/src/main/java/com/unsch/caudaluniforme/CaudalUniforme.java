424031f72a1a297db5cf7f5d64114fdb4e8372c4
Jduchman <jorge.duchman.27@unsch.edu.pe>
2023-01-22 22:40:19 -0500

    cargando proyecto

--- CaudalUniforme/src/main/java/com/unsch/caudaluniforme/CaudalUniforme.java
+++ CaudalUniforme/src/main/java/com/unsch/caudaluniforme/CaudalUniforme.java
@@ -3,13 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.unsch.caudal;
+package com.unsch.caudaluniforme;
 
 /**
  *
  * @author Duchman
  */
-public class Caudal {
+public class CaudalUniforme {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
